e162adc72a875a588da07339509cdde2dd37b733
liam stabeno and shane stafford <shane@>
2017-03-20 16:04:20 -0700

    complete the rectangle java spark exercise

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,36 @@
-import java.io.Console;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    Console console = System.console();
-    System.out.println("Enter length of your rectangle yo");
-    String userLength = console.readLine();
-    int intLength = Integer.parseInt(userLength);
-    System.out.println("Enter width of your rectangle yo");
-    String userWidth = console.readLine();
-    int intWidth = Integer.parseInt(userWidth);
-    Rectangle userRectangle = new Rectangle(intLength, intWidth);
-    boolean squareResult = userRectangle.isSquare();
-    System.out.println("True or false: your rectangle is also a square: " + squareResult + "! Cool.");
+
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object>  model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl" );
+      String userSide1 = request.queryParams("side1");
+      String userSide2 = request.queryParams("side2");
+
+      if (userSide1 == null && userSide2 == null){
+        userSide1 = "0";
+        userSide2 = userSide1;
+      }
+
+      int side1 = Integer.parseInt(userSide1);
+      int side2 = Integer.parseInt(userSide2);
+      Rectangle userRectangle = new Rectangle(side1, side2);
+      List<Rectangle> allRectangles = new ArrayList<Rectangle>();
+      allRectangles.add(userRectangle);
+      model.put("rectangles", allRectangles);
+      boolean squareResult = userRectangle.isSquare();
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
